5527149dcaf89b37db9ad2f2aae5f7ac0daabc60
Nitay Caspi <NitayCaspi12@gmail.com>
2021-11-16 11:45:20 +0200

    change to RTK configureStore

--- MN-react/src/redux/store.js
+++ MN-react/src/redux/store.js
@@ -1,27 +1,15 @@
-import { createStore, combineReducers, applyMiddleware, compose } from "redux";
-import thunk from "redux-thunk";
-
-import userReducer from "./reducers/userReducer";
-import dataReducer from "./reducers/dataReducer";
-import uiReducer from "./reducers/uiReducer";
-
-const initialState = {};
-
-const middleware = [thunk];
-
-const reducers = combineReducers({
+import { configureStore } from '@reduxjs/toolkit'
+import userReducer from './reducers/userReducer'
+import dataReducer from './reducers/dataReducer'
+import uiReducer from './reducers/uiReducer'
+const reducer = {
   user: userReducer,
   data: dataReducer,
   UI: uiReducer,
-});
+}
+const store = configureStore({
+  reducer,
+  devTools: process.env.NODE_ENV !== 'production',
+})
 
-const store = createStore(
-  reducers,
-  initialState,
-  compose(
-    applyMiddleware(...middleware),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-  )
-);
-
-export default store;
+export default store
