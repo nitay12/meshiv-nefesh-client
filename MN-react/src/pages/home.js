6c8214a342203845af59bb0049ab87cfce1af0a4
Nitay Caspi <NitayCaspi12@gmail.com>
2021-11-17 10:59:30 +0200

    change to functinoal component

--- MN-react/src/pages/home.js
+++ MN-react/src/pages/home.js
@@ -1,47 +1,34 @@
-import React, { Component } from "react";
-import { PropTypes } from "prop-types";
+import React, { useEffect } from "react";
 // MUI imports
 import Grid from "@material-ui/core/grid";
 // Components
 import Answer from "../components/Answer";
 import Profile from "../components/Profile";
 // Redux
-import { connect } from "react-redux";
-import { getAnswers } from "../redux/actions/dataActions";
+import { useSelector, useDispatch } from "react-redux";
+import { dataSelector } from "../redux/slices/dataSlice";
+import { getAllAnswers } from "../redux/actions/rtkDataActions";
 
-export class home extends Component {
-  componentDidMount() {
-    this.props.getAnswers()
-  }
-  render() {
-    const { answers, loading} = this.props.data
-    let recentAnswersMarkup = !loading ? (
-      answers.map((answer) => (
-        <Answer key={answer.answerId} answer={answer} />)
-      )
-    ) : (
-      <p>loading...</p>
-    );
-    return (
-      <Grid container spacing={2}>
-        <Grid item sm={6} xs={12}>
-          <Profile />
-        </Grid>
-        <Grid item sm={6} xs={12}>
-          {recentAnswersMarkup}
-        </Grid>
+function Home() {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(getAllAnswers());
+  },[]);
+  const { answers, loading } = useSelector(dataSelector);
+  let recentAnswersMarkup = loading ? (
+    <p>loading...</p>
+  ) : (
+    answers.map((answer) => <Answer key={answer.answerId} answer={answer} />)
+  );
+  return (
+    <Grid container spacing={2}>
+      <Grid item sm={6} xs={12}>
+        <Profile />
       </Grid>
-    );
-  }
+      <Grid item sm={6} xs={12}>
+        {recentAnswersMarkup}
+      </Grid>
+    </Grid>
+  );
 }
-
-home.propTypes = {
-  getAnswers: PropTypes.func.isRequired,
-  data: PropTypes.object.isRequired,
-}
-
-const mapStateToProps = (state) => ({
-  data: state.data
-})
-
-export default connect(mapStateToProps, {getAnswers})(home);
+export default Home;
