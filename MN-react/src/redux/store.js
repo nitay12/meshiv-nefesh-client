7a462bd158611b99b866296b1192eac75d7d1239
Nitay Caspi <NitayCaspi12@gmail.com>
2021-11-17 11:02:28 +0200

    add dataSlice reducer

--- MN-react/src/redux/store.js
+++ MN-react/src/redux/store.js
@@ -2,10 +2,12 @@ import { configureStore } from '@reduxjs/toolkit'
 import userReducer from './reducers/userReducer'
 import dataReducer from './reducers/dataReducer'
 import uiReducer from './reducers/uiReducer'
+import dataSlice from './slices/dataSlice'
 const reducer = {
   user: userReducer,
   data: dataReducer,
   UI: uiReducer,
+  rtkData: dataSlice,
 }
 const store = configureStore({
   reducer,
